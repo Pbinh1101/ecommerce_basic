a93264e8efbf836f1b4ab99f41586cd9585f47bc
unknown <binhnguyen110120@gmail.com>
2021-11-27 20:44:52 +0700

    crate Update base_UI

--- index.php
+++ index.php
@@ -21,8 +21,49 @@
 </head>
 
 <body>
+     <!-- Search Wrapper Area Start -->
+    <div class="search-wrapper section-padding-100">
+        <div class="search-close">
+            <i class="fa fa-close" aria-hidden="true"></i>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="search-content">
+                        <form action="#" method="get">
+                            <input type="search" name="search" id="search" placeholder="Type your keyword...">
+                            <button type="submit"><img src="./assets/img/core-img/search.png" alt=""></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Search Wrapper Area End -->
+
+    <!-- ##### Main Content Wrapper Start ##### -->
+    <div class="main-content-wrapper d-flex clearfix">
+
+        <!-- Mobile Nav (max width 767px)-->
+        <div class="mobile-nav">
+            <!-- Navbar Brand -->
+            <div class="amado-navbar-brand">
+                <a href="index.php"><img src="./assets/img/core-img/logo.png" alt=""></a>
+            </div>
+            <!-- Navbar Toggler -->
+            <div class="amado-navbar-toggler">
+                <span></span><span></span><span></span>
+            </div>
+         </div>  
+  
+
     
-    <?php require("./inc/header.php") ?>
+         <header class="header-area clearfix">
+            <?php require("./inc/header.php") ?>
+
+            <?php require("./inc/sidebar.php") ?>
+        </header>
+   
 
         <!-- Product Catagories Area Start -->
         <div class="products-catagories-area clearfix">
